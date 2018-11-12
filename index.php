a3376bfa59ca58f1ac4d1482d83b08afcd93b0f1
Fabio Urso <furso@wmc.ch>
2018-11-12 16:18:50 +0100

    Test Commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//Seiten Parameter setzen
+$page_titel = "Herzlich Willkommen zu meinem Ressourcenplanungstool";
+$page_kurztitel = "Home";
+$page_content = "text_home.php";
+
+//Template includen
+include("includes/template.inc.php");
+?>
