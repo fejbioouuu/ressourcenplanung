d63e8f2a365946275776e7255e264168d3aad383
Fabio Urso <furso@wmc.ch>
2019-02-18 16:21:36 +0100

    Changed Layout & add descriptions

---
+++ class/Absencetype.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fabio
+ * Date: 18.02.2019
+ * Time: 15:00
+ */
+
+class Absencetype
+{
+private $id;
+private $absence_name;
+private $absence_type;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAbsenceName()
+    {
+        return $this->absence_name;
+    }
+
+    /**
+     * @param mixed $absence_name
+     */
+    public function setAbsenceName($absence_name)
+    {
+        $this->absence_name = $absence_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAbsenceType()
+    {
+        return $this->absence_type;
+    }
+
+    /**
+     * @param mixed $absence_type
+     */
+    public function setAbsenceType($absence_type)
+    {
+        $this->absence_type = $absence_type;
+    }
+
+}
