0be35b335ceea761882c9fe5a74c6ca02b9a7cf6
Samuel Gfeller <sgfeller@wmc.ch>
2019-04-08 15:03:21 +0200

    added sql select and options with employees in select

--- content/edit_absences.php
+++ content/edit_absences.php
@@ -5,17 +5,25 @@
  * Date: 18.03.2019
  * Time: 10:58
  */
+require_once 'class/Employee.php';
 
-$connect = getCon();
-
-$sqlEmp = "SELECT id, Vorname, Name from employees";
-
-$stmt = $connect->prepare($sqlEmp);
-
-$stmt->execute();
-
-$users = $stmt->fetch();
-
+function getAllEmployees() {
+	$connect = getCon();
+	$sqlEmp = "SELECT id, Vorname, Name from employees";
+	$result = $connect->query($sqlEmp);
+	if ($result->num_rows > 0) {
+		$employees = [];
+		while ($row = $result->fetch_assoc()) {
+			$employee = new Employee();
+			$employee->setId($row['id']);
+			$employee->setVorname($row['Vorname']);
+			$employee->setName($row['Name']);
+			$employees[] = $employee;
+		}
+		return $employees;
+	}
+	return null;
+}
 ?>
 
 
@@ -24,14 +32,11 @@ $users = $stmt->fetch();
 
 
 <form action="absences.php" method="post">
-    <input type="number" name="Id" placeholder="Id" value="<?php echo $id;?>"><br><br>
-    <select>
-        <?php foreach ($users as $user): ?>
-            <option value="<?php $user['id']; ?>"><?php $user['name']; ?> ok </option>
-                <?php endforeach; ?>
-
-
-
+    <input type="number" name="Id" placeholder="Id" value="<?php ?>"><br><br>
+    <select name="empId">
+        <?php foreach (getAllEmployees() as $employee){ ?>
+            <option value="<?php echo $employee->getId(); ?>"> <?php echo $employee->getVorname().' '.$employee->getName(); ?> </option>
+                <?php } ?>
     </select>
         <div>
         <input type="submit" name="insert" value="Add">
@@ -39,5 +44,5 @@ $users = $stmt->fetch();
         <input type="submit" name="delete"  value="Delete">
         <input type="submit" name="search" value="Find">
     </div>
-
+    <input type="submit">
 </form>
