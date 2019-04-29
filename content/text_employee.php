7fec462ba964bfbd6f1eb7b2aed067080ec65f39
Fabio Urso <fejbioouuu@gmail.com>
2019-04-29 15:09:04 +0200

    add javascript

--- content/text_employee.php
+++ content/text_employee.php
@@ -25,21 +25,52 @@ echo '<table><tr>
   </tr>';
 if ($result->num_rows > 0) {
 
-    while ($row = $result->fetch_assoc()) {
-        echo '<tr><td>' . $row['id'] . '</td>
-<td>' . $row['Vorname'] . '</td>
-<td>' . $row['Name'] . '</td>
-<td>' . $row['Anstellungsverhaeltnis'] . '</td>
-<td>
-    <progress id="progressBar'.$row['id'].'" value="0" max="' . $row['Pensum'] . '">' . '</progress>
-    <span id="status'.$row['id'].'"></span><script>fillProgressBar('.$row['id'].');</script>
-</td>
-<td>' . $row['Vertragsbeginn'] . '</td>
-<td>' . $row['Vertragsende'] . '</td></tr>';
-    }
-    echo '</table>';
+while ($row = $result->fetch_assoc()) {
+echo '<tr><td>' . $row['id'] . '</td>
+    <td>' . $row['Vorname'] . '</td>
+    <td>' . $row['Name'] . '</td>
+    <td>' . $row['Anstellungsverhaeltnis'] . '</td>
+    <td>
+        <div id="myProgress">
+            <div id="myBar'.$row['id'].'" class="progressBar" data-id="'.$row['id'].'" data-pensum="'.$row['Pensum'].'">data->0%</div>        
+        </div>
+       
+       
+
+    </td>
+    <td>' . $row['Vertragsbeginn'] . '</td>
+    <td>' . $row['Vertragsende'] . '</td></tr>';
+
+}
+
+
+echo '</table>';
 } else {
-    echo '</table><br><h2>No Results</h2>';
+echo '</table><br><h2>No Results</h2>';
 }
 
 ?>;
+<!---->
+<!--<progress id="progressBar'.$row['id'].'" value="0" max="' . $row['Pensum'] . '">' . '</progress>-->
+<!--<span id="status'.$row['id'].'"></span><script>fillProgressBar('.$row['id'].');</script>-->
+
+<!--if ($result->num_rows > 0) {-->
+<!---->
+<!--while ($row = $result->fetch_assoc()) {-->
+<!--echo '<tr><td>' . $row['id'] . '</td>-->
+<!--    <td>' . $row['Vorname'] . '</td>-->
+<!--    <td>' . $row['Name'] . '</td>-->
+<!--    <td>' . $row['Anstellungsverhaeltnis'] . '</td>-->
+<!--    <td>-->
+<!--        <progress id="progressBar'.$row['id'].'" value="0" max="' . $row['Pensum'] . '">' . '</progress>-->
+<!--        <span id="status'.$row['id'].'"></span><script>fillProgressBar('.$row['id'].');</script>-->
+<!--    </td>-->
+<!--    <td>' . $row['Vertragsbeginn'] . '</td>-->
+<!--    <td>' . $row['Vertragsende'] . '</td></tr>';-->
+<!--}-->
+<!--echo '</table>';-->
+<!--} else {-->
+<!--echo '</table><br><h2>No Results</h2>';-->
+<!--}-->
+<!---->
+<!--?>;-->
--- script/bar.js
+++ script/bar.js
@@ -1,64 +1,95 @@
+window.onload = function () {
+    let bars = document.getElementsByClassName("progressBar");
 
-function fillProgressBar(id){
-    let bar = document.getElementById('progressBar'+id);
-    let status = document.getElementById('status'+id);
+    Array.prototype.forEach.call(bars, function(bar) {
 
 
-    // status.innerHTML = bar.max+"%";
-    // bar.value = bar.max;
-    let i=0;
-    // alert('while '+i+'<'+bar.max);
-    // while (i < parseInt(bar.max)) {
-    for (i;i<bar.max;i++) {
-        setTimeout(function () {
-            status.innerHTML = i + "%";
-            bar.value = i;
-            // alert('in timeout');
+        move(bar.dataset.id);
+    });
 
-            // animateProgress(i, sim)
-        }, 20000);
+};
+
+function move(id) {
+    var elem = document.getElementById("myBar"+id);
+
+
+    //start bei 0%
+    var width = 0;
+    var inter = setInterval(frame, 10);
+
+    function frame() {
+        if (width >= elem.dataset.pensum) {
+            clearInterval(inter);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+            elem.innerHTML = width * 1 + '%';
+        }
     }
-
-console.log(id,i);
-
-    // }
-    // animateProgress(0,bar,status);
-
 }
 
-function animateProgress(i,sim) {
-
-    if(i == bar.max || i===100){
-        // status.innerHTML = bar.max;
-        // bar.value = bar.max;
-        clearTimeout(sim);
-        return;
-    }
-
-
-    i++;
-
-}
-
-
-function progressBar(progress) {
-    // alert(progress);
-    var bar = document.getElementById('progressBar');
-    var status = document.getElementById('status');
-    status.innerHTML = progress+"%";
-    bar.value = progress;
-    progress++;
-    var sim = setTimeout(progressBar(progress),30);
-
-
-
-
-    // if(progress == 100){
-    //     status.innerHTML = "100%";
-    //     bar.value = 100;
-    //     clearTimeout(sim);
-        // var finalMessage = document.getElementById('finalMessage');
-        // finalMessage.innerHTML = "Process is complete";
+//
+// function fillProgressBar(id){
+//     let bar = document.getElementById('progressBar'+id);
+//     let status = document.getElementById('status'+id);
+//
+//
+//     // status.innerHTML = bar.max+"%";
+//     // bar.value = bar.max;
+//     let i=0;
+//     // alert('while '+i+'<'+bar.max);
+//     // while (i < parseInt(bar.max)) {
+//     for (i;i<bar.max;i++) {
+//         setTimeout(function () {
+//             status.innerHTML = i + "%";
+//             bar.value = i;
+//             // alert('in timeout');
+//
+//             // animateProgress(i, sim)
+//         }, 20000);
 //     }
-}
+//
+// console.log(id,i);
+//
+//     // }
+//     // animateProgress(0,bar,status);
+//
+// }
+//
+// function animateProgress(i,sim) {
+//
+//     if(i == bar.max || i===100){
+//         // status.innerHTML = bar.max;
+//         // bar.value = bar.max;
+//         clearTimeout(sim);
+//         return;
+//     }
+//
+//
+//     i++;
+//
+// }
+//
+//
+// function progressBar(progress) {
+//     // alert(progress);
+//     var bar = document.getElementById('progressBar');
+//     var status = document.getElementById('status');
+//     status.innerHTML = progress+"%";
+//     bar.value = progress;
+//     progress++;
+//     var sim = setTimeout(progressBar(progress),30);
+
+
+// ---
+
+
+// if(progress == 100){
+//     status.innerHTML = "100%";
+//     bar.value = 100;
+//     clearTimeout(sim);
+// var finalMessage = document.getElementById('finalMessage');
+// finalMessage.innerHTML = "Process is complete";
+//     }
+// }
 
